4a388cc03f646e32472c4f69ec35b8c2d3b6ee58
ga12lil <agrigoren@sfedu.ru>
2024-02-18 17:49:01 +0300

    Add tests

---
+++ bot/src/test/java/edu/java/bot/command/ListCommandTest.java
@@ -0,0 +1,64 @@
+package edu.java.bot.command;
+
+import edu.java.bot.repository.User;
+import com.pengrad.telegrambot.model.Chat;
+import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.Update;
+import edu.java.bot.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class ListCommandTest {
+
+    @Test
+    public void handleEmptyTrackList() {
+        User user = mock(User.class);
+        List<String> links = new ArrayList<>();
+        when(user.getTrackLinks()).thenReturn(links);
+        UserRepository repository = mock(UserRepository.class);
+        when(repository.getUser(any())).thenReturn(user);
+
+        Update update = mock(Update.class);
+        Chat chat = mock(Chat.class);
+        when(chat.id()).thenReturn(5L);
+        Message message = mock(Message.class);
+        when(message.chat()).thenReturn(chat);
+        when(update.message()).thenReturn(message);
+
+        ListCommand listCommand = new ListCommand(repository);
+
+        assertEquals("You don't have tracking links. Use /track"
+                , listCommand.handle(update).getParameters().get("text"));
+    }
+
+    @Test
+    public void handleNotEmptyTrackList() {
+        User user = mock(User.class);
+        List<String> links = new ArrayList<>();
+        links.add("Track1");
+        links.add("Track2");
+        when(user.getTrackLinks()).thenReturn(links);
+        UserRepository repository = mock(UserRepository.class);
+        when(repository.getUser(any())).thenReturn(user);
+
+        Update update = mock(Update.class);
+        Chat chat = mock(Chat.class);
+        when(chat.id()).thenReturn(5L);
+        Message message = mock(Message.class);
+        when(message.chat()).thenReturn(chat);
+        when(update.message()).thenReturn(message);
+
+        ListCommand listCommand = new ListCommand(repository);
+
+        assertEquals("Tracking links:\n- Track1\n- Track2\n"
+                , listCommand.handle(update).getParameters().get("text"));
+    }
+
+}
