2b134f7feebbbcd7b5d5afd8b81d8bac28626da9
ga12lil <agrigoren@sfedu.ru>
2024-03-02 15:25:35 +0300

    fix test

--- bot/src/test/java/edu/java/bot/command/HelpCommandTest.java
+++ bot/src/test/java/edu/java/bot/command/HelpCommandTest.java
@@ -2,25 +2,30 @@ package edu.java.bot.command;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HelpCommandTest extends AbstractTest {
     @Autowired
     HelpCommand helpCommand;
-    private final String correctResponseString =
-            """
-            /list — Write tracking resources
-            /track — Start tracking
-            /untrack — Stop tracking a link
-            /start — Start dialog
-            /help — List of available commands
-            """;
+    private final List<String> correctResponseStrings = List.of(
+            "/list — Write tracking resources",
+            "/track — Start tracking",
+            "/untrack — Stop tracking a link",
+            "/start — Start dialog",
+            "/help — List of available commands"
+    );
 
     @Test
     void shouldReturnAllCommands() {
         Map<String, Object> params = helpCommand.handle(getMockUpdate()).getParameters();
-        assertEquals(params.get("text"),
-                correctResponseString);
+        List<String> responseCommands = Arrays.stream(
+                params.get("text").toString().split("\n"))
+                .toList();
+        assertTrue(responseCommands.containsAll(correctResponseStrings));
     }
 }
