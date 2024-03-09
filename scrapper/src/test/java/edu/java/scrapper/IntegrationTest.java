a24146f330531d7d54f03884afb146b87c66373b
ga12lil <agrigoren@sfedu.ru>
2024-03-09 15:07:27 +0300

    add Test for migrations

--- scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/IntegrationTest.java
@@ -1,12 +1,29 @@
 package edu.java.scrapper;
 
+import edu.java.ScrapperApplication;
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.DirectoryResourceAccessor;
+import liquibase.resource.ResourceAccessor;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Testcontainers
+@SpringBootTest(classes = ScrapperApplication.class)
 public abstract class IntegrationTest {
     public static PostgreSQLContainer<?> POSTGRES;
 
@@ -21,7 +38,22 @@ public abstract class IntegrationTest {
     }
 
     private static void runMigrations(JdbcDatabaseContainer<?> c) {
-        // ...
+        try (Connection connection = DriverManager.getConnection(
+                c.getJdbcUrl(),
+                c.getUsername(),
+                c.getPassword()
+        )) {
+            Database database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection));
+            var migrationsPath = new File(".")
+                    .toPath().toAbsolutePath()
+                    .getParent().getParent()
+                    .resolve("migrations");
+            ResourceAccessor resourceAccessor = new DirectoryResourceAccessor(migrationsPath);
+            Liquibase liquibase = new Liquibase("master.xml", resourceAccessor, database);
+            liquibase.update(new Contexts(), new LabelExpression());
+        } catch (SQLException | FileNotFoundException | LiquibaseException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @DynamicPropertySource
---
+++ scrapper/src/test/java/edu/java/scrapper/MigrationTest.java
@@ -0,0 +1,45 @@
+package edu.java.scrapper;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MigrationTest extends IntegrationTest {
+    private static final String GET_ALL_TABLES = """
+            SELECT table_name
+            FROM information_schema.tables
+            WHERE table_schema='public' AND table_catalog = current_database()
+            """;
+
+    @SneakyThrows
+    @Test
+    void tablesShouldBeCreated(){
+        Connection connection = DriverManager.getConnection(
+                POSTGRES.getJdbcUrl(),
+                POSTGRES.getUsername(),
+                POSTGRES.getPassword()
+        );
+
+        ResultSet allTables = connection.createStatement().executeQuery(GET_ALL_TABLES);
+
+        assertThat(allTablesToList(allTables)).containsAll(necessaryTables());
+    }
+
+    private List<String> allTablesToList(ResultSet allTables) throws SQLException {
+        List<String> result = new ArrayList<>();
+        while(allTables.next()){
+            result.add(allTables.getString("TABLE_NAME"));
+        }
+        return result;
+    }
+
+    private List<String> necessaryTables(){
+        return List.of("link", "chat", "subscription");
+    }
+}
