9795e707e796f1330e8cbdffece1c3b4d57be897
ga12lil <agrigoren@sfedu.ru>
2024-04-14 19:30:04 +0300

    add logs

--- scrapper/src/main/java/edu/java/kafka/ScrapperQueueProducer.java
+++ scrapper/src/main/java/edu/java/kafka/ScrapperQueueProducer.java
@@ -3,12 +3,14 @@ package edu.java.kafka;
 import edu.java.dto.bot.LinkUpdateRequest;
 import edu.java.httpclient.botclient.BotClient;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 @ConditionalOnProperty(prefix = "app", name = "useQueue", havingValue = "true")
 public class ScrapperQueueProducer implements BotClient {
@@ -17,5 +19,6 @@ public class ScrapperQueueProducer implements BotClient {
 
     public void postUpdate(LinkUpdateRequest update) {
         kafkaTemplate.send(messagesTopic.name(), update);
+        log.info("send update to Kafka queue!");
     }
 }
